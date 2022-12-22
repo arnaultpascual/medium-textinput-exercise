a37018d696e23d8f8bdaed9337148501598ffa31
Arnault Pascual <arnault.pascual.webmaster@gmail.com>
2022-12-22 15:52:09 +0100

    add edit and save contact "basic" logic
    add edit and save contact "basic" logic

---
+++ app/src/main/java/com/pascualarnault/contactformexersise/Contact.kt
@@ -0,0 +1,13 @@
+package com.pascualarnault.contactformexersise
+
+class Contact {
+    var firstname : String = "Cunegonde"
+    var lastname : String = "Johnson"
+    var company : String = "Digital System & Co"
+    var phone : String = "0612345678"
+    var landline : String = "0123456789"
+    var mail : String = "cunegonde.johnson@gmail.com"
+    var address : String = "3199-3121 Woodbine Ave, Cleveland"
+    var zipcode : String = "OH 44113"
+    var description = ""
+}
--- app/src/main/java/com/pascualarnault/contactformexersise/MainActivity.kt
+++ app/src/main/java/com/pascualarnault/contactformexersise/MainActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.util.Patterns
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.textfield.TextInputLayout
 import com.pascualarnault.contactformexersise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    private var contact = Contact()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +25,88 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         title = "Edit Contact"
+        setupListeners()
         phoneValidator()
         zipCodeValidator()
         emailValidator()
+        setEditTextFromContact()
     }
 
-    private fun getPhoneValidatorWatcher(textInputLayout: com.google.android.material.textfield.TextInputLayout) : TextWatcher {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_edit -> {
+                switchEditableState(true)
+                item.setIcon(R.drawable.close)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setContact(){
+        contact.firstname    = binding.firstNameTL.editText!!.text.toString()
+        contact.lastname    = binding.lastNameTL.editText!!.text.toString()
+        contact.company     = binding.companyNameTL.editText!!.text.toString()
+        contact.phone       = binding.phoneTL.editText!!.text.toString()
+        contact.landline    = binding.landlineTL.editText!!.text.toString()
+        contact.mail        = binding.mailTL.editText!!.text.toString()
+        contact.address     = binding.addressTL.editText!!.text.toString()
+        contact.zipcode     = binding.zipCodeTL.editText!!.text.toString()
+        contact.description = binding.descriptionTL.editText!!.text.toString()
+    }
+
+    private fun setEditTextFromContact(){
+        binding.firstNameTL.editText!!.text = getEditableFromString(contact.firstname)
+        binding.lastNameTL.editText!!.text = getEditableFromString(contact.lastname)
+        binding.companyNameTL.editText!!.text = getEditableFromString(contact.company)
+        binding.phoneTL.editText!!.text = getEditableFromString(contact.phone)
+        binding.landlineTL.editText!!.text = getEditableFromString(contact.landline)
+        binding.mailTL.editText!!.text = getEditableFromString(contact.mail)
+        binding.addressTL.editText!!.text = getEditableFromString(contact.address)
+        binding.zipCodeTL.editText!!.text = getEditableFromString(contact.zipcode)
+        binding.descriptionTL.editText!!.text = getEditableFromString(contact.description)
+    }
+
+    private fun getEditableFromString(string: String) : Editable {
+        return Editable.Factory.getInstance().newEditable(string)
+    }
+
+    private fun switchEditableState(isEditable: Boolean){
+        binding.firstNameTL.isEnabled    = isEditable
+        binding.lastNameTL.isEnabled    = isEditable
+        binding.companyNameTL.isEnabled = isEditable
+        binding.phoneTL.isEnabled       = isEditable
+        binding.landlineTL.isEnabled    = isEditable
+        binding.mailTL.isEnabled        = isEditable
+        binding.addressTL.isEnabled     = isEditable
+        binding.zipCodeTL.isEnabled     = isEditable
+        binding.descriptionTL.isEnabled = isEditable
+
+        if (isEditable){
+            binding.cancelEdit.visibility = View.VISIBLE
+            binding.confirmEdit.visibility = View.VISIBLE
+        }else{
+            binding.cancelEdit.visibility = View.GONE
+            binding.confirmEdit.visibility = View.GONE
+        }
+    }
+
+    private fun setupListeners(){
+        binding.cancelEdit.setOnClickListener {
+            switchEditableState(false)
+        }
+        binding.confirmEdit.setOnClickListener {
+            setContact()
+            switchEditableState(false)
+        }
+    }
+
+    private fun getPhoneValidatorWatcher(textInputLayout: TextInputLayout) : TextWatcher {
         return object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
